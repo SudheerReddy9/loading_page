c9d9468c6e439a8e819a7a7808171d24cdb56df1
SudheerReddy9 <pokalasudheer143@gmail.com>
2025-02-13 12:54:49 -0500

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Welcome to flutter world'),
+          ],
+        ),
+      ),
+    );
+  }
+}
