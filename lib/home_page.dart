1faa0620c0641d8db04053937e33074e4d07b61d
SudheerReddy9 <pokalasudheer143@gmail.com>
2025-02-14 10:01:34 -0500

    Final comit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -14,11 +15,179 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
+      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Welcome to flutter world'),
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.1,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 31, 179, 83),
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(1),
+              ),
+              child: const Center(child: Text("Material App",textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.1,
+              decoration: BoxDecoration(
+                color: const Color(0xff7c94b6),
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(1),
+              ),
+              child: const Center(child: Text("Scaffold",textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.28,
+                height: MediaQuery.of(context).size.height * 0.1,
+                decoration: BoxDecoration(
+                  color: const Color(0xff7c94b6),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+                child: const Center(child: Text("AppBar",textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.28,
+                height: MediaQuery.of(context).size.height * 0.1,
+                decoration: BoxDecoration(
+                  color: const Color(0xff7c94b6),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+                child: const Center(
+                    child: Text(
+                  "Container",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.28,
+                height: MediaQuery.of(context).size.height * 0.1,
+                decoration: BoxDecoration(
+                  color: const Color(0xff7c94b6),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+                child: const Center(child: Text("FAB",textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )),
+              ),
+            ]),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.1,
+              decoration: BoxDecoration(
+                color: const Color(0xff7c94b6),
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(1),
+              ),
+              child: const Center(child: Text("Column",textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.28,
+                height: MediaQuery.of(context).size.height * 0.1,
+                decoration: BoxDecoration(
+                  color:  const Color.fromARGB(255, 84, 144, 227),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+                child: const Center(child: Text("AppBar",textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.28,
+                height: MediaQuery.of(context).size.height * 0.1,
+                decoration: BoxDecoration(
+                  color:  const Color.fromARGB(255, 84, 144, 227),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+                child: const Center(
+                    child: Text(
+                  "Container",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.28,
+                height: MediaQuery.of(context).size.height * 0.1,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 84, 144, 227),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+                child: const Center(child: Text("FAB",textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )),
+              ),
+            ]),
           ],
         ),
       ),
